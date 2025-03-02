5d007879357b7e47f00cf1ac6265631a4c6a05f2
6qlz <6qlzluv@gmail.com>
2025-03-02 21:29:32 +0300

    code revamped

--- 6qlzHunter.py
+++ 6qlzHunter.py
@@ -8,6 +8,7 @@ import subprocess
 import time
 from Modules.report_generator import ReportGenerator
 from packaging import version
+from concurrent.futures import ThreadPoolExecutor
 
 def clear_terminal():
     """Clears the terminal screen."""
@@ -21,220 +22,70 @@ banner = fr"""
 / /_/ / /_/ / / / /_/ __  / /_/ / / / / /____/ / /    
 \____/\__, /_/ /___/_/ /_/\__,_/_/ /_/\__/____/_/     
         /_/                                           
-   
+
    Made by @6qlz                     v.1
 """
 
+def read_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return [line.strip() for line in file.readlines() if line.strip()]
+    except Exception as e:
+        print(Fore.RED + f"Error reading file: {str(e)}")
+        return None
+
 def payloads(prompt: str):
     location = os.path.join(os.path.dirname(__file__), "Prompts", f"{prompt}.txt")
-    with open(location, "r") as file:
-        prompts = [line.strip() for line in file.readlines() if line.strip()]
-        payload = "\n".join(prompts).replace('"', '\\"')
-    return f'"{payload}"'
+    return "\n".join(read_file(location)).replace('"', '\\"')
 
-def target_list(file: str):
-    try:
-        with open(file, "r") as file:
-            targets = [x.strip() for x in file.readlines()]
-        return targets
-    except Exception as e:
-        print(Fore.RED + f"Error reading target list file: {str(e)}")
-        start()
-
-def run_scan_with_progress(input_file: str, payload: str, scan_type: str):
-    print(Fore.CYAN + f"\nStarting {scan_type} scan..." + Style.RESET_ALL)
-    with tqdm(total=100,
-              desc="Scanning",
-              bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)) as pbar:
-        output = scan(f"nuclei -list {input_file} -ai {payload} -silent")
-        for i in range(100):
-            time.sleep(0.1)
-            pbar.update(1)
+def run_scan(target, payload, scan_type):
+    output = scan(f"nuclei -target {target} -ai {payload} -silent")
     if output:
         results = parse_nuclei_output(output)
         report_gen = ReportGenerator()
         report_gen.generate_report(results, scan_type, ["html", "json", "csv"])
-        return output # Return output for further use if needed
+        return results
     return None
 
-def emails():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("emails")
-    output = run_scan_with_progress(input_file, payload, "Emails")
-    if not output:
-        print(Fore.RED + "No results found")
+def run_scan_with_progress(targets, payload, scan_type):
+    print(Fore.CYAN + f"\nStarting {scan_type} scan..." + Style.RESET_ALL)
+    with tqdm(total=len(targets),
+              desc="Scanning",
+              bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)) as pbar:
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(run_scan, target, payload, scan_type) for target in targets]
+            for future in futures:
+                future.result()
+                pbar.update(1)
 
-def low_hanging_fruits():
+def scan_type_function(scan_type, prompt):
     input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("lowhangingfruits")
-    output = run_scan_with_progress(input_file, payload, "Low Hanging Fruits")
-    if not output:
-        print(Fore.RED + "No results found")
+    targets = read_file(input_file)
+    if targets:
+        payload = payloads(prompt)
+        run_scan_with_progress(targets, payload, scan_type)
     else:
-        parse_nuclei_output(output)
+        print(Fore.RED + "No targets found")
 
-def sensitive_data_exposure():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("sensitivedataexposure")
-    output = run_scan_with_progress(input_file, payload, "Sensitive Data Exposure")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
+def check_for_updates():
+    current_version = "1.0.0"  # Update this to the current version of your tool
+    repo_owner = "6qlz"
+    repo_name = "6qlzHunt3r"
 
-def sql_injection():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("sqli")
-    output = run_scan_with_progress(input_file, payload, "SQL Injection")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
+    try:
+        response = requests.get(f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest")
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        latest_release = response.json()
+        latest_version = latest_release["tag_name"]
+        release_notes = latest_release["body"]
 
-def cross_site_scripting():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("xss")
-    output = run_scan_with_progress(input_file, payload, "Cross Site Scripting")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def server_side_request_forgery():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("ssrf")
-    output = run_scan_with_progress(input_file, payload, "Server Side Request Forgery")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def local_and_remote_file_inclusion():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("fileinclusion")
-    output = run_scan_with_progress(input_file, payload, "Local and Remote File Inclusion")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def command_injection():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("commandinjection")
-    output = run_scan_with_progress(input_file, payload, "Command Injection")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def xxe():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("xxe")
-    output = run_scan_with_progress(input_file, payload, "XXE")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def host_header_injection():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("hostheaderinjection")
-    output = run_scan_with_progress(input_file, payload, "Host Header Injection")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def cloud_security_issues():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("cloudsecurity")
-    output = run_scan_with_progress(input_file, payload, "Cloud Security Issues")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def web_cache_poisoning():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("webcachepoisoning")
-    output = run_scan_with_progress(input_file, payload, "Web Cache Poisoning")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def security_misconfigurations():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("securitymisconfig")
-    output = run_scan_with_progress(input_file, payload, "Security Misconfigurations")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def hardcoded_credentials():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("hardcodedcredentials")
-    output = run_scan_with_progress(input_file, payload, "Hardcoded Credentials")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-
-def deserialization():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("deserialization")
-    output = run_scan_with_progress(input_file, payload, "Deserialization")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def idor():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("idor")
-    output = run_scan_with_progress(input_file, payload, "IDOR")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def race_condition():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("racecondition")
-    output = run_scan_with_progress(input_file, payload, "Race Condition")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
-
-def websocket():
-    input_file = input(Fore.WHITE + "Enter the target list file: ")
-    targets = target_list(input_file)
-    payload = payloads("websocket")
-    output = run_scan_with_progress(input_file, payload, "WebSocket")
-    if not output:
-        print(Fore.RED + "No results found")
-    else:
-        parse_nuclei_output(output)
+        if version.parse(latest_version) > version.parse(current_version):
+            print(Fore.GREEN + f"\nA new version of 6qlzHunt3r is available: {latest_version}" + Style.RESET_ALL)
+            print(Fore.GREEN + f"Release Notes:\n{release_notes}\n" + Style.RESET_ALL)
+        else:
+            print(Fore.CYAN + "\nYou are using the latest version of 6qlzHunt3r." + Style.RESET_ALL)
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + f"\nFailed to check for updates: {str(e)}" + Style.RESET_ALL)
 
 def start():
     try:
@@ -259,48 +110,38 @@ def start():
             print (Fore.RED + "  [" + Fore.WHITE + "15" + Fore.RED + "]" + Fore.WHITE + " IDOR")
             print (Fore.RED + "  [" + Fore.WHITE + "16" + Fore.RED + "]" + Fore.WHITE + " Race Condition")
             print (Fore.RED + "  [" + Fore.WHITE + "17" + Fore.RED + "]" + Fore.WHITE + " WebSocket")
+            print (Fore.RED + "  [" + Fore.WHITE + "98" + Fore.RED + "]" + Fore.WHITE + " Check for Updates")
             print (Fore.RED + "  [" + Fore.WHITE + "99" + Fore.RED + "]" + Fore.WHITE + " Exit")
             print ("\n")
 
             prompt = input(Fore.RED + "6qlzHunt3r~" + Fore.WHITE + "# ")
 
-            if prompt == "00":
-                low_hanging_fruits()
-            elif prompt == "01":
-                sensitive_data_exposure()
-            elif prompt == "02":
-                sql_injection()
-            elif prompt == "03":
-                cross_site_scripting()
-            elif prompt == "04":
-                server_side_request_forgery()
-            elif prompt == "05":
-                local_and_remote_file_inclusion()
-            elif prompt == "06":
-                command_injection()
-            elif prompt == "07":
-                xxe()
-            elif prompt == "08":
-                host_header_injection()
-            elif prompt == "09":
-                cloud_security_issues()
-            elif prompt == "10":
-                web_cache_poisoning()
-            elif prompt == "11":
-                emails()
-            elif prompt == "12":
-                security_misconfigurations()
-            elif prompt == "13":
-                hardcoded_credentials()
-            elif prompt == "14":
-                deserialization()
-            elif prompt == "15":
-                idor()
-            elif prompt == "16":
-                race_condition()
-            elif prompt == "17":
-                websocket()
-            elif prompt == "99" or prompt == "99" or prompt == "exit" or prompt == "quit":
+            scan_type_map = {
+                "00": ("Low Hanging Fruits", "lowhangingfruits"),
+                "01": ("Sensitive Data Exposure", "sensitivedataexposure"),
+                "02": ("SQL Injection", "sqli"),
+                "03": ("Cross Site Scripting", "xss"),
+                "04": ("Server Side Request Forgery", "ssrf"),
+                "05": ("File Inclusion", "fileinclusion"),
+                "06": ("Command Injection", "commandinjection"),
+                "07": ("XML External Entity", "xxe"),
+                "08": ("Host Header Injection", "hostheaderinjection"),
+                "09": ("Cloud Security Issues", "cloudsecurity"),
+                "10": ("Web Cache Poisoning", "webcachepoisoning"),
+                "11": ("Emails", "emails"),
+                "12": ("Security Misconfigurations", "securitymisconfig"),
+                "13": ("Hardcoded Credentials", "hardcodedcredentials"),
+                "14": ("Deserialization", "deserialization"),
+                "15": ("IDOR", "idor"),
+                "16": ("Race Condition", "racecondition"),
+                "17": ("WebSocket", "websocket")
+            }
+
+            if prompt in scan_type_map:
+                scan_type_function(*scan_type_map[prompt])
+            elif prompt == "98":
+                check_for_updates()
+            elif prompt in ["99", "exit", "quit"]:
                 break
             else:
                 print(Fore.RED + "Invalid option")
